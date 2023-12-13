64e74407c7b17d5af7eb0a193898c578005d8129
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-12-13 14:00:08 -0600

    database created

---
+++ data/migrations/20231213194416_initial.js
@@ -0,0 +1,57 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('projects', t => {
+    t.increments('project_id')
+    t.string('project_name')
+      .notNullable()
+    t.string('project_description')
+    t.boolean('project_completed')
+      .default(false)
+  })
+  .createTable('resources', t => {
+    t.increments('resource_id')
+    t.string('resource_name')
+      .notNullable()
+      .unique()
+    t.string('resource_description')
+  })
+  .createTable('tasks', t => {
+    t.increments('task_id')
+    t.string('task_description')
+      .notNullable()
+    t.string('task_notes')
+    t.boolean('task_completed')
+      .default(false)
+    t.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('project_id')
+      .inTable('projects')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+  })
+  .createTable('project_resources', t => {
+    t.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('project_id')
+      .inTable('projects')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    t.integer('resource_id')
+      .unsigned()
+      .notNullable()
+      .references('resource_id')
+      .inTable('resources')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    t.primary(['project_id', 'resource_id'])
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+};
