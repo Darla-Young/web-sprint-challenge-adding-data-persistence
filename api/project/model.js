03df4abf2d4a4f0b93bdd3143b5d4939586aa328
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-12-14 07:36:36 -0600

    project post model complete

--- api/project/model.js
+++ api/project/model.js
@@ -5,15 +5,11 @@ module.exports = {
   getProjects,
 }
 
-function addProject (project) {
-  /* response example:
-{
-  "project_id":1,
-  "project_name":"bar",
-  "project_description":null,
-  "project_completed":false
-}
-  */
+async function addProject (project) {
+  const id = await db('projects').insert(project)
+  const newProject = await db('projects').where('project_id', id[0])
+
+  return newProject[0]
 }
 
 function getProjects () {
