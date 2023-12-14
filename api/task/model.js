4a05d167b8844b5706f1c346d3dcb983c1e2f2ea
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-12-14 08:18:17 -0600

    complete

--- api/task/model.js
+++ api/task/model.js
@@ -5,29 +5,42 @@ module.exports = {
   getTasks,
 }
 
-function addTask (task) {
-  // each task must include a name & description
-  
-  /* response example:
-{
-  "task_id":1,
-  "task_description":"baz",
-  "task_notes":null,
-  "task_completed":false,
-  "project_id":1
-}
-  */
+function makeBool (int) {
+  return int === 0 ? false : true
 }
 
-function getTasks () {
-  /* response example:
-[{
-  "task_id":1,
-  "task_description":"baz",
-  "task_notes":null,
-  "task_completed":false,
-  "project_name:"bar",
-  "project_description":null
-}]
-  */
+async function addTask (task) {
+  const id = await db('tasks').insert(task)
+  const arr = await db('tasks').where('task_id', id[0])
+
+  const newTask = {
+    ...arr[0],
+    task_completed: makeBool(arr[0].task_completed)
+  }
+
+  return newTask
+}
+
+async function getTasks () {
+  const arr = await db('tasks as t')
+    .join('projects as p', 't.project_id', '=', 'p.project_id')
+    .select(
+      'task_id', 
+      'task_description', 
+      'task_notes', 
+      'task_completed', 
+      'project_name', 
+      'project_description',
+    )
+    
+  const fixedArr = []
+
+  arr.forEach(obj => {
+    fixedArr.push({
+      ...obj,
+      task_completed: makeBool(obj.task_completed)
+    })
+  })
+
+  return fixedArr
 }
